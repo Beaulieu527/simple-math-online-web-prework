4307eaad38c17f5fe3c1c1a2cac1137d6cee1d17
beaulieudev <beaulieudev@gmail.com>
2018-10-04 01:50:31 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,26 @@
 def addition(num1, num2)
-  puts 10 - 5
+  puts 1 + 4
 end
 
 def subtraction(num1, num2)
-  puts "#{num1} - #{num2}"
-  return num1 - num2
-end
+  puts 10 -5
 
 def division(num1, num2)
-   puts "#{num1} / #{num2}"
+   puts 50 / 2
 end
 
 def multiplication(num1, num2)
-   puts "#{num1} * #{num2}"
+   puts 4 * 30
 end
 
 def modulo(num1, num2)
-  puts "#{num1} % #{num2}"
+  puts 34 % 5
 end
 
 def square_root(num)
-    puts Math.sqrt("#{num}")
+    puts Math.sqrt(9)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  puts  "#{num1} + (( #{num2} * #{num3} ) / #{num4} )"
+  puts  7 + (( 43 * 23 ) / 83 )
 end
